f02ad842bbf41f50b932446cdd350a62565fcc4c
Tran Trong Nghia <emladevops@protonmail.com>
2024-05-22 23:50:22 +0700

    add my public gpg key

--- app/layout.tsx
+++ app/layout.tsx
@@ -73,13 +73,7 @@ export default function RootLayout({
 
   useEffect(() => {
     initParticlesEngine(async (engine) => {
-      // you can initiate the tsParticles instance (engine) here, adding custom shapes or presets
-      // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
-      // starting from v2 you can add only the features you need reducing the bundle size
-      //await loadAll(engine);
-      //await loadFull(engine);
       await loadSlim(engine);
-      //await loadBasic(engine);
     }).then(() => {
       setInit(true);
     });
